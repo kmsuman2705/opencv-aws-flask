d3f2067704bab8f3fa92aaf309e9c919ffc88a8d
suman mishra <kmsuman27@gmail.com>
2024-08-27 15:21:23 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, Response
+import cv2
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# Replace with your phone's IP camera URL
+camera_url = "http://10.33.227.235:8080/video"
+camera = cv2.VideoCapture(camera_url)
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            # Encode the frame in JPEG format
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
